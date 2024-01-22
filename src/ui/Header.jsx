8ded6e1fb2b4247bd062c29ba3ec9e7f39b0a31e
Angela Bellò <angela.bello90@gmail.com>
2024-01-22 15:39:31 +0100

    +loader function for data fetching/loading state

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Angela</p>
+    </header>
+  );
+}
+
+export default Header;
